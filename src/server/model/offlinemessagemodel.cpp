3299afe48f2b5f2673f456f8ede8f1e79c6f5b83
siqing647 <siqingliu07@gmail.com>
2024-07-20 17:46:09 +0800

    重构完毕，使用动态链接库，增加新的model类作为所有model的父类

---
+++ src/server/model/offlinemessagemodel.cpp
@@ -0,0 +1,47 @@
+
+#include "offlinemessagemodel.hpp"
+
+// 存储用户的离线消息
+void OfflineMsgModel::insert(int userid, string msg) {
+    // 1. 组装 sql 语句
+    char sql[1024] = { 0 };
+    sprintf(sql, "insert into offlinemessage values(%d, '%s')", userid,
+        msg.c_str());
+
+    shared_ptr<Connection> sp = _connPool->getConnection();
+    if (sp)
+        sp->update(sql);
+}
+
+// 删除用户的离线消息
+void OfflineMsgModel::remove(int userid) {
+    // 1. 组装 sql 语句
+    char sql[1024] = { 0 };
+    sprintf(sql, "delete from offlinemessage where userid=%d", userid);
+
+    shared_ptr<Connection> sp = _connPool->getConnection();
+    if (sp)
+        sp->update(sql);
+}
+
+// 查询用户的离线消息
+vector<string> OfflineMsgModel::query(int userid) {
+    // 1. 组装 sql 语句
+    char sql[1024] = { 0 };
+    sprintf(sql, "select message from offlinemessage where userid = %d", userid);
+
+    vector<string> vec;
+    shared_ptr<Connection> sp = _connPool->getConnection();
+    if (sp) {
+        MYSQL_RES* res = sp->query(sql);
+        if (res != nullptr) {
+            // 把 userid 用户的所有离线消息放入 vec 中返回
+            MYSQL_ROW row;
+            while ((row = mysql_fetch_row(res)) != nullptr)
+                vec.push_back(row[0]);
+            mysql_free_result(res);
+            return vec;
+        }
+    }
+    return vec;
+}
