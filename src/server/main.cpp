5f20ffd440cd8565d8ba8c67eebd8574f5beae2f
siqing647 <siqingliu07@gmail.com>
2024-07-22 16:50:28 +0800

    整理 网络模块和业务模块的主类

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -6,28 +6,28 @@ using namespace std;
 
 // 处理服务器 ctrl+c 结束后，重置 user 的状态信息
 void resetHandler(int) {
-  ChatService::instance()->reset();
-  exit(0);
+    ChatService::instance()->reset();
+    exit(0);
 }
 
-int main(int argc, char **argv) {
-  if (argc < 3) {
-    cerr << "command invalid! example: ./ChatServer 127.0.0.1 6000" << endl;
-    exit(-1);
-  }
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        cerr << "command invalid! example: ./ChatServer 127.0.0.1 6000" << endl;
+        exit(-1);
+    }
 
-  // 解析通过命令行参数传递的 ip 和 port
-  char *ip = argv[1];
-  uint16_t port = atoi(argv[2]);
+    // 解析通过命令行参数传递的 ip 和 port
+    char* ip = argv[1];
+    uint16_t port = atoi(argv[2]);
 
-  signal(SIGINT, resetHandler);
+    signal(SIGINT, resetHandler);
 
-  EventLoop loop;
-  InetAddress addr(ip, port);
-  ChatServer server(&loop, addr, "ChatServer");
+    EventLoop loop;
+    InetAddress addr(ip, port);
+    ChatServer server(&loop, addr, "ChatServer");
 
-  server.start();
-  loop.loop();
+    server.start();
+    loop.loop();
 
-  return 0;
+    return 0;
 }
