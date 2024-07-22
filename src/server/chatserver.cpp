06a1a50e55ee23370705890f1437739b50375041
siqing647 <siqingliu07@gmail.com>
2024-07-22 16:48:57 +0800

    Model 数据层代码框架，映射 User 表的 ORM 类，整理 User 表的数据操作类 UserModel 中的 insert 方法

--- src/server/chatserver.cpp
+++ src/server/chatserver.cpp
@@ -1,3 +1,4 @@
+
 #include "chatserver.hpp"
 #include "chatservice.hpp"
 #include "json.hpp"
@@ -10,47 +11,47 @@ using namespace placeholders;
 using json = nlohmann::json;
 
 // 初始化聊天服务器对象
-ChatServer::ChatServer(EventLoop *loop, const InetAddress &listenAddr,
-                       const string &nameArg)
+ChatServer::ChatServer(EventLoop* loop, const InetAddress& listenAddr,
+    const string& nameArg)
     : _server(loop, listenAddr, nameArg), _loop(loop) {
-  // 注册连接回调
-  _server.setConnectionCallback(std::bind(&ChatServer::onConnection, this, _1));
+    // 注册连接回调
+    _server.setConnectionCallback(std::bind(&ChatServer::onConnection, this, _1));
 
-  // 注册消息回调
-  _server.setMessageCallback(
-      std::bind(&ChatServer::onMessage, this, _1, _2, _3));
+    // 注册消息回调
+    _server.setMessageCallback(
+        std::bind(&ChatServer::onMessage, this, _1, _2, _3));
 
-  // 设置线程数量
-  _server.setThreadNum(4);
+    // 设置线程数量
+    _server.setThreadNum(4);
 }
 
 // 启动服务
 void ChatServer::start() { _server.start(); }
 
 // 上报连接相关信息的回调函数
-void ChatServer::onConnection(const TcpConnectionPtr &conn) {
-  if (!conn->connected()) { // 客户端断开链接
-    ChatService::instance()->clientCloseException(conn);
-    conn->shutdown();
-  }
+void ChatServer::onConnection(const TcpConnectionPtr& conn) {
+    if (!conn->connected()) { // 客户端断开链接
+        ChatService::instance()->clientCloseException(conn);
+        conn->shutdown();
+    }
 }
 
 // 上报读写事件相关信息的回调函数
-void ChatServer::onMessage(const TcpConnectionPtr &conn, Buffer *buffer,
-                           Timestamp time) {
-  string buf = buffer->retrieveAllAsString();
+void ChatServer::onMessage(const TcpConnectionPtr& conn, Buffer* buffer,
+    Timestamp time) {
+    string buf = buffer->retrieveAllAsString();
 
-  // 测试，添加json打印代码
-  // cout << buf << endl;
+    // 测试，添加json打印代码
+    // cout << buf << endl;
 
-  // 数据的反序列化
-  json js = json::parse(buf);
+    // 数据的反序列化
+    json js = json::parse(buf);
 
-  // 作用：完全解耦网络模块的代码和业务模块的代码
-  // 通过 js["msgid"] 的 get<int>() 方法获取消息类型
-  // 根据消息类型，返回对应的业务处理器（就是一个回调（函数对象））
-  auto msgHandler = ChatService::instance()->getHandler(js["msgid"].get<int>());
+    // 作用：完全解耦网络模块的代码和业务模块的代码
+    // 通过 js["msgid"] 的 get<int>() 方法获取消息类型
+    // 根据消息类型，返回对应的业务处理器（就是一个回调（函数对象））
+    auto msgHandler = ChatService::instance()->getHandler(js["msgid"].get<int>());
 
-  // 通过事先绑定好的回调函数，来执行相应的业务处理
-  msgHandler(conn, js, time);
+    // 通过事先绑定好的回调函数，来执行相应的业务处理
+    msgHandler(conn, js, time);
 }
