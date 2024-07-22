97b5f0f83c11b38c4316d32a2e7222498a2d3f75
siqing647 <siqingliu07@gmail.com>
2024-07-22 16:33:16 +0800

    重构完毕，使用动态链接库，增加新的model类作为所有model的父类

--- include/server/model/offlinemessagemodel.hpp
+++ include/server/model/offlinemessagemodel.hpp
@@ -1,3 +1,4 @@
+
 #ifndef OFFLINEMESSAGEMODEL_H
 #define OFFLINEMESSAGEMODEL_H
 
@@ -9,14 +10,14 @@ using namespace std;
 // 提供离线消息表的操作接口方法
 class OfflineMsgModel : public model {
 public:
-  // 存储用户的离线消息
-  void insert(int userid, string msg);
+	// 存储用户的离线消息
+	void insert(int userid, string msg);
 
-  // 删除用户的离线消息
-  void remove(int userid);
+	// 删除用户的离线消息
+	void remove(int userid);
 
-  // 查询用户的离线消息
-  vector<string> query(int userid);
+	// 查询用户的离线消息
+	vector<string> query(int userid);
 };
 
 #endif
