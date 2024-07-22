4776992276361e55b38c81adc2536fbbfe5c9ac4
siqing647 <siqingliu07@gmail.com>
2024-07-22 16:33:56 +0800

    重构完毕，使用动态链接库，增加新的model类作为所有model的父类

--- include/server/model/usermodel.hpp
+++ include/server/model/usermodel.hpp
@@ -1,3 +1,4 @@
+
 #ifndef USERMODEL_H
 #define USERMODEL_H
 
@@ -7,17 +8,17 @@
 // User 表的数据操作类
 class UserModel : public model {
 public:
-  // User 表的增加方法
-  bool insert(User &user);
+	// User 表的增加方法
+	bool insert(User& user);
 
-  // 根据用户号码查询用户信息
-  User query(int id);
+	// 根据用户号码查询用户信息
+	User query(int id);
 
-  // 更新用户的状态信息
-  bool updateState(User user);
+	// 更新用户的状态信息
+	bool updateState(User user);
 
-  // 重置用户的状态信息
-  void resetState();
+	// 重置用户的状态信息
+	void resetState();
 };
 
 #endif
