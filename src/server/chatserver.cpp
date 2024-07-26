06f10b8c09a71826bcb87d5caef4a8a3695321f2
siqing647 <siqingliu07@gmail.com>
2024-07-26 14:48:26 +0800

    格式化 注释

--- src/server/chatserver.cpp
+++ src/server/chatserver.cpp
@@ -17,12 +17,10 @@ ChatServer::ChatServer(EventLoop* loop, const InetAddress& listenAddr,
     // 注册连接回调
     _server.setConnectionCallback(std::bind(&ChatServer::onConnection, this, _1));
 
-    // 注册消息回调
-    _server.setMessageCallback(
+    _server.setMessageCallback( // 注册消息回调
         std::bind(&ChatServer::onMessage, this, _1, _2, _3));
 
-    // 设置线程数量
-    _server.setThreadNum(4);
+    _server.setThreadNum(4); // 设置线程数量
 }
 
 // 启动服务
@@ -44,8 +42,7 @@ void ChatServer::onMessage(const TcpConnectionPtr& conn, Buffer* buffer,
     // 测试，添加json打印代码
     // cout << buf << endl;
 
-    // 数据的反序列化
-    json js = json::parse(buf);
+    json js = json::parse(buf); // 数据的反序列化
 
     // 作用：完全解耦网络模块的代码和业务模块的代码
     // 通过 js["msgid"] 的 get<int>() 方法获取消息类型
